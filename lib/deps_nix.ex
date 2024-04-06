0d17fa35b8688cd81f6fea516c5cead8cffecb3d
Andrew Bruce <me@andrewbruce.net>
2024-04-06 10:36:19 +0100

    mix new deps_nix

---
+++ lib/deps_nix.ex
@@ -0,0 +1,18 @@
+defmodule DepsNix do
+  @moduledoc """
+  Documentation for `DepsNix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DepsNix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/deps_nix_test.exs
@@ -0,0 +1,8 @@
+defmodule DepsNixTest do
+  use ExUnit.Case
+  doctest DepsNix
+
+  test "greets the world" do
+    assert DepsNix.hello() == :world
+  end
+end
