e6c2b4cc84133705c4786e9df61ded84f21e267a
Andrew Bruce <me@andrewbruce.net>
2024-04-21 09:42:10 +0100

    "release" command, as yet unused!

--- shell.nix
+++ shell.nix
@@ -3,9 +3,24 @@
 with pkgs;
 
 mkShell {
-  packages = [
-    elixir_1_16
-    (elixir_ls.override { elixir = elixir_1_16; })
-    mix2nix # for comparison
-  ];
+  packages =
+    let
+      release = writeShellApplication {
+        name = "release";
+        runtimeInputs = [ elixir_1_16 gh ];
+        text = ''
+          tag=$1
+
+          gh auth login
+          gh release create "$tag" --draft --generate-notes
+          mix hex.publish
+        '';
+      };
+    in
+    [
+      elixir_1_16
+      (elixir_ls.override { elixir = elixir_1_16; })
+      mix2nix # for comparison
+      release
+    ];
 }
