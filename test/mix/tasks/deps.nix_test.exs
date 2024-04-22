74f536fa0c07b926f8d6df8742e1cbae3abbe9ed
Andrew Bruce <me@andrewbruce.net>
2024-04-22 17:12:01 +0100

    Ensure output is nixpkgs-fmt formatted

--- test/mix/tasks/deps.nix_test.exs
+++ test/mix/tasks/deps.nix_test.exs
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Deps.NixTest do
   use ExUnit.Case, async: true
 
-  test "produces a Nix function for the fixture app's dependencies" do
+  test "produces a formatted Nix function for the fixture app's dependencies" do
     assert {_, 0} =
              System.shell("mix deps.nix --env prod",
                cd: "fixtures/example",
@@ -10,6 +10,13 @@ defmodule Mix.Tasks.Deps.NixTest do
 
     assert {"«lambda @ " <> _, 0} =
              System.shell("nix eval --file deps.nix 2> /dev/null",
+               cd: "fixtures/example",
+               into: "",
+               lines: 1024
+             )
+
+    assert {_, 0} =
+             System.shell("nixpkgs-fmt --check deps.nix 2> /dev/null",
                cd: "fixtures/example",
                into: "",
                lines: 1024
