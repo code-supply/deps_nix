fa796db3b72bc84b0b5d7920b52daadac200df59
Andrew Bruce <me@andrewbruce.net>
2024-05-27 09:47:25 +0100

    Extend timeout of formatting test
    This can take a long time in CI, because the fixture dir doesn't have
    its dependencies until this test is run.

--- test/mix/tasks/deps.nix_test.exs
+++ test/mix/tasks/deps.nix_test.exs
@@ -1,6 +1,7 @@
 defmodule Mix.Tasks.Deps.NixTest do
   use ExUnit.Case, async: false
 
+  @tag timeout: 120_000
   test "produces a formatted Nix function for the fixture app's dependencies" do
     {run_output, run_status} =
       System.shell("mix do deps.get, deps.nix --env prod 2>&1",
