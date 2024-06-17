95e13dd367bf8c6b43a087767d77dfae1e5a05ef
Andrew Bruce <me@andrewbruce.net>
2024-06-17 11:19:20 +0100

    StreamData 1.1.1
    Squashes Elixir 1.17.x deprecations

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule DepsNix.MixProject do
     [
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:stream_data, "~> 0.6.0", only: [:dev, :test]}
+      {:stream_data, "~> 1.1", only: [:dev, :test]}
     ]
   end
 
