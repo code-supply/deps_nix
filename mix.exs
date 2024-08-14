b84f6915f9b2ef806ccd8e94f108d465a0ef7a29
Andrew Bruce <me@andrewbruce.net>
2024-08-14 15:22:49 +0100

    Bump patch version for PR #4

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule DepsNix.MixProject do
       elixir: "~> 1.16",
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.5.1",
+      version: "0.5.2",
 
       # Docs
       source_url: @scm_url,
