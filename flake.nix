6e3099b87913b50f09dbdab77115436e0c929717
Andrew Bruce <me@andrewbruce.net>
2025-01-08 17:40:27 +0000

    Use latest Elixir (1.18.x)

--- flake.nix
+++ flake.nix
@@ -8,34 +8,12 @@
     let
       forAllSystems =
         generate:
-        nixpkgs.lib.genAttrs
-          [
-            "aarch64-darwin"
-            "x86_64-darwin"
-            "aarch64-linux"
-            "x86_64-linux"
-          ]
-          (
-            system:
-            generate {
-              pkgs = import nixpkgs {
-                inherit system;
-                overlays = [
-                  (
-                    self: super:
-                    let
-                      beamPackages = with super.beam_minimal; packagesWith interpreters.erlang_27;
-                    in
-                    {
-                      inherit beamPackages;
-                      erlang = beamPackages.erlang_27;
-                      elixir = beamPackages.elixir_1_17;
-                    }
-                  )
-                ];
-              };
-            }
-          );
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "x86_64-linux"
+        ] (system: generate { pkgs = import nixpkgs { inherit system; }; });
     in
     {
       packages = forAllSystems ({ pkgs, ... }: (pkgs.callPackages ./fixtures/example/deps.nix { }));
