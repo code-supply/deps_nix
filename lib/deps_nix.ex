c7c8dadea6598cf3ff36d0a0b24affa805bd5bbb
Andrew Bruce <me@andrewbruce.net>
2025-01-15 18:23:24 +0000

    Inline default-overrides.nix
    This has caused formatting issues on projects with different nix
    formatters.

--- lib/deps_nix.ex
+++ lib/deps_nix.ex
@@ -275,7 +275,33 @@ defmodule DepsNix do
         };
 
         defaultOverrides = (
-      #{default_overrides()}
+          final: prev:
+
+          let
+            apps = {
+              explorer = [
+                {
+                  name = "rustlerPrecompiled";
+                  toolchain = {
+                    name = "nightly-2024-07-26";
+                    sha256 = "sha256-5icy5hSaQy6/fUim9L2vz2GeZNC3fX1N5T2MjnkTplc=";
+                  };
+                }
+              ];
+            };
+
+            applyOverrides =
+              appName: drv:
+              let
+                allOverridesForApp = builtins.foldl' (
+                  acc: workaround: acc // (workarounds.${workaround.name} workaround) drv
+                ) { } apps.${appName};
+
+              in
+              if builtins.hasAttr appName apps then drv.override allOverridesForApp else drv;
+
+          in
+          builtins.mapAttrs applyOverrides prev
         );
 
         self = packages // (defaultOverrides self packages) // (overrides self packages);
@@ -290,14 +316,6 @@ defmodule DepsNix do
       self
       """
     }
-  end
-
-  defp default_overrides do
-    "#{:code.priv_dir(:deps_nix)}/default-overrides.nix"
-    |> File.read!()
-    |> String.trim_trailing()
-    |> Util.indent()
-    |> Util.indent()
   end
 
   defp indent_deps("") do
