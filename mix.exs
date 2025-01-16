38ed1b84d1d71e4616e1cd798e61e1609739ba16
Andrew Bruce <me@andrewbruce.net>
2025-01-16 10:39:34 +0000

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule DepsNix.MixProject do
       elixir: "~> 1.16",
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "2.0.0",
+      version: "2.1.0",
 
       # Docs
       source_url: @scm_url,
