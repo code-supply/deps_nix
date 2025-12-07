2796aa399f4fb44784b1ccfb2820ea7f9ca19df8
Andrew Bruce <me@andrewbruce.net>
2025-12-07 11:48:34 +0000

    Double ExUnit timeout
    Failing in CI due to recompilation taking its time.

--- test/mix/tasks/deps.nix_test.exs
+++ test/mix/tasks/deps.nix_test.exs
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Deps.NixTest do
   use ExUnit.Case, async: true
 
-  @tag timeout: 240_000
+  @tag timeout: :timer.minutes(8)
   test "produces a formatted Nix function for the fixture app's dependencies" do
     {run_output, run_status} =
       System.shell("mix do deps.get, deps.nix --env prod 2>&1",
